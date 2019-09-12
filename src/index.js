884f1a340dd3f403922f5f1e2b69fcaa548b38a6
XX <fallwall19@gmail.com>
2019-09-11 22:07:06 -0400

    mongodb setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://rnstrava:passwordpassword@cluster0-m4snj.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo instance', err);
+});
+
 
 app.get('/', (req, res) => {
   res.send('hi hi hi!');
