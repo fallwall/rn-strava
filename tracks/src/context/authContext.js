8591a01f404d49be03b3cb4013233e7f5b123550
XX <fallwall19@gmail.com>
2019-09-26 16:31:35 -0400

    actions in context

--- tracks/src/context/authContext.js
+++ tracks/src/context/authContext.js
@@ -7,9 +7,27 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // code
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // code
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // code
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signup, signin, signout },
   {
     isSignedIn: false,
   },
