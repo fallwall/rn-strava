98deda9ddb4a87a7ff133b026a48e6686e9d4186
XX <fallwall19@gmail.com>
2019-10-01 23:58:17 -0400

    requestPermissionsAsync

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync } from 'expo-loaction';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text h3>
         TrackCreate Screen Here
     </Text>
       <Map />
+      {err ? <Text>Please enable location service</Text> : null}
     </SafeAreaView>
   )
 };
