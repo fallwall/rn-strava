684e712088c6099e22bb2c51ac0b6d45decd6612
XX <fallwall19@gmail.com>
2019-10-03 15:37:18 -0400

    track detail screen

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
-import { StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import { ListItem } from 'react-native-elements';
-import { Context as TrackContext } from '../context/TrackContext';
 import { NavigationEvents } from 'react-navigation';
+import { Context as TrackContext } from '../context/TrackContext';
 
-const TrackListScreen = () => {
+const TrackListScreen = ({ navigation }) => {
   const { state, fetchTracks } = useContext(TrackContext);
 
   return (
@@ -17,12 +22,15 @@ const TrackListScreen = () => {
         data={state}
         keyExtractor={item => item._id}
         renderItem={({ item }) => {
-          return (<TouchableOpacity>
-            <ListItem
-              chevron={true}
-              title={item.name}
-            />
-          </TouchableOpacity>
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate('TrackDetail', { _id: item._id })}
+            >
+              <ListItem
+                chevron={true}
+                title={item.name}
+              />
+            </TouchableOpacity>
           );
         }}
       />
