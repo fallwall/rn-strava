db1d52b7d28179eb235d6c33af889685b3128116
XX <fallwall19@gmail.com>
2019-10-03 12:01:35 -0400

    start tracking button/track form

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -4,6 +4,7 @@ import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
+import TrackForm from '../components/TrackForm';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 
@@ -18,6 +19,7 @@ const TrackCreateScreen = ({ isFocused }) => {
         TrackCreate Screen
     </Text>
       <Map />
+      <TrackForm />
       {err ? <Text>Please enable location service</Text> : null}
     </SafeAreaView>
   );
