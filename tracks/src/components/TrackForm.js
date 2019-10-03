d381eab1fe2d683f49cffcdbec33f86a7300924c
XX <fallwall19@gmail.com>
2019-10-03 14:03:46 -0400

    save button hookup

--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
   const {
@@ -10,6 +11,7 @@ const TrackForm = () => {
     stopRecording,
     changeName,
   } = useContext(LocationContext);
+  const [saveTrack] = useSaveTrack;
 
   return (
     <>
@@ -37,6 +39,7 @@ const TrackForm = () => {
         {
           !recording && locations.length
             ? (<Button
+              onPress={saveTrack}
               title="Save"
             />)
             : null
---
+++ tracks/src/hooks/useSaveTrack.js
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { Context as LocationContext } from '../context/LocationContext';
+import { Context as TrackContext } from '../context/TrackContext';
+
+export default () => {
+  const { createTrack } = useContext(TrackContext);
+  const { state: { name, locations } } = useContext(LocationContext);
+
+  const saveTrack = () => {
+    createTrack(name, locations);
+  };
+
+  return [saveTrack];
+};
